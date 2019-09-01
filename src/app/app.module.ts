289cf70387fe24422459eea056da25c74b392e96
Adithya Sreyaj <adi.sreyaj@gmail.com>
2019-09-01 23:35:07 +0530

    Replaced component with the ng-star-rating package for demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RatingBarComponent } from './rating-bar/rating-bar.component';
 import { UserRatingComponent } from './cards/user-rating/user-rating.component';
 import { FoodRatingComponent } from './cards/food-rating/food-rating.component';
-import { StarRatingModule } from 'star-rating';
+import { StarRatingModule } from '@sreyaj/ng-star-rating';
 @NgModule({
-  declarations: [
-    AppComponent,
-    RatingBarComponent,
-    UserRatingComponent,
-    FoodRatingComponent
-  ],
+  declarations: [AppComponent, UserRatingComponent, FoodRatingComponent],
   imports: [BrowserModule, AppRoutingModule, StarRatingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
